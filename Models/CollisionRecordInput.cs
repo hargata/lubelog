6ffa5cb356e7fa565d06648dc3fe3b3f99bc4c9c
ivancheahhh <ivancheahkf@gmail.com>
2024-01-03 10:11:54 -0700

    renamed.

--- Models/CollisionRecordInput.cs
+++ Models/CollisionRecordInput.cs
@@ -10,6 +10,6 @@
         public decimal Cost { get; set; }
         public string Notes { get; set; }
         public List<UploadedFiles> Files { get; set; } = new List<UploadedFiles>();
-        public CollisionRecord ToServiceRecord() { return new CollisionRecord { Id = Id, VehicleId = VehicleId, Date = DateTime.Parse(Date), Cost = Cost, Mileage = Mileage, Description = Description, Notes = Notes, Files = Files }; }
+        public CollisionRecord ToCollisionRecord() { return new CollisionRecord { Id = Id, VehicleId = VehicleId, Date = DateTime.Parse(Date), Cost = Cost, Mileage = Mileage, Description = Description, Notes = Notes, Files = Files }; }
     }
 }
