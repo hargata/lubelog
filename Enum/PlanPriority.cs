2eb9a58aa6c8a9fc5ffa00c567aaec1e8a5e8a7a
DESKTOP-GENO133\IvanPlex <ivancheahkf@gmail.com>
2024-01-19 10:27:17 -0700

    added enums.

---
+++ Enum/PlanPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCareTracker.Enum
+{
+    public enum PlanPriority
+    {
+        Critical = 0,
+        Normal = 1,
+        Low = 2
+    }
+}
---
+++ Enum/PlanProgress.cs
@@ -0,0 +1,10 @@
+﻿namespace CarCareTracker.Enum
+{
+    public enum PlanProgress
+    {
+        Backlog = 0,
+        InProgress = 1,
+        Testing = 2,
+        Done = 3
+    }
+}
