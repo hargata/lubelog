04f90ae6a8490f6e36fa90a87c63b132db25bbed
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2024-02-16 13:55:22 -0700

    Added Distance Traveled Chart

--- Models/Report/CostForVehicleByMonth.cs
+++ Models/Report/CostForVehicleByMonth.cs
@@ -5,5 +5,8 @@
         public int MonthId { get; set; }
         public string MonthName { get; set; }
         public decimal Cost { get; set; }
+        public int MaxMileage { get; set; }
+        public int MinMileage { get; set; }
+        public int DistanceTraveled { get; set; }
     }
 }
