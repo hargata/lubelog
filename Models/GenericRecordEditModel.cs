0d8822c49647ceb5307599eeb5a22290bd35a399
DESKTOP-GENO133\IvanPlex <ivancheahkf@gmail.com>
2024-02-21 20:54:14 -0700

    reorganized folders.

---
+++ Models/GenericRecordEditModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarCareTracker.Models
+{
+    public class GenericRecordEditModel
+    {
+        public ImportMode DataType { get; set; }
+        public List<int> RecordIds { get; set; } = new List<int>();
+    }
+}
