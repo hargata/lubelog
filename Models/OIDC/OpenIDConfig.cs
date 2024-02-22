a2c013ec43b48e21e27d4342cb0ce0808d1e2b4c
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2024-02-22 15:59:35 -0700

    added state for OIDC

--- Models/OIDC/OpenIDConfig.cs
+++ Models/OIDC/OpenIDConfig.cs
@@ -9,6 +9,7 @@
         public string TokenURL { get; set; }
         public string RedirectURL { get; set; }
         public string Scope { get; set; }
-        public string RemoteAuthURL { get { return $"{AuthURL}?client_id={ClientId}&response_type=code&redirect_uri={RedirectURL}&scope={Scope}"; } }
+        public string State { get { return Guid.NewGuid().ToString().Substring(0, 8); } }
+        public string RemoteAuthURL { get { return $"{AuthURL}?client_id={ClientId}&response_type=code&redirect_uri={RedirectURL}&scope={Scope}&state={State}"; } }
     }
 }
