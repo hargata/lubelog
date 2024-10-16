4f36e603408e89729784b7fe2d62449434f2782b
RescueRangers <48625686+RescueRangers@users.noreply.github.com>
2024-10-16 14:24:13 +0200

    Add a user setting to hide the tags field when entering a new odometer value

--- Models/UserConfig.cs
+++ Models/UserConfig.cs
@@ -12,6 +12,7 @@
         public bool EnableRootUserOIDC { get; set; }
         public bool HideZero { get; set; }
         public bool UseUKMPG {get;set;}
+        public bool HideOdometerTags { get; set; }
         public bool UseThreeDecimalGasCost { get; set; }
         public bool UseMarkDownOnSavedNotes { get; set; }
         public bool EnableAutoReminderRefresh { get; set; }
