44f13723dee75b56c3d60e8cffedff6ab4ac58d8
RescueRangers <48625686+RescueRangers@users.noreply.github.com>
2024-11-21 15:18:14 +0100

    Add MVP data access required for pagination

--- External/Interfaces/IGasRecordDataAccess.cs
+++ External/Interfaces/IGasRecordDataAccess.cs
@@ -5,6 +5,8 @@ namespace CarCareTracker.External.Interfaces
     public interface IGasRecordDataAccess
     {
         public List<GasRecord> GetGasRecordsByVehicleId(int vehicleId);
+        public List<GasRecord> GetGasRecordsByVehicleIdPaginated(int vehicleId, int from, int to);
+        public int GetGasRecordsByVehicleIdCount(int vehicleId);
         public GasRecord GetGasRecordById(int gasRecordId);
         public bool DeleteGasRecordById(int gasRecordId);
         public bool SaveGasRecordToVehicle(GasRecord gasRecord);
