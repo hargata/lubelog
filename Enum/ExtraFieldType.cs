a5c42877c9d17a636cacf146b8e0eb5c296f19bc
RescueRangers <48625686+RescueRangers@users.noreply.github.com>
2025-04-18 13:27:21 +0200

    Somehow ExtraFieldType got cleared during merge

---
+++ Enum/ExtraFieldType.cs
@@ -0,0 +1,12 @@
+namespace CarCareTracker.Models
+{
+    public enum ExtraFieldType
+    {
+        Text = 0,
+        Number = 1,
+        Decimal = 2,
+        Date = 3,
+        Time = 4,
+        Location = 5
+    }
+}
