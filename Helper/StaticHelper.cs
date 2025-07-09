4731615ac555a95750e4b7a0796d8b4e4d4cc08f
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2025-07-09 10:25:20 -0600

    bump version

--- Helper/StaticHelper.cs
+++ Helper/StaticHelper.cs
@@ -12,7 +12,7 @@ namespace CarCareTracker.Helper
     /// </summary>
     public static class StaticHelper
     {
-        public const string VersionNumber = "1.4.8";
+        public const string VersionNumber = "1.4.9";
         public const string DbName = "data/cartracker.db";
         public const string UserConfigPath = "data/config/userConfig.json";
         public const string ServerConfigPath = "data/config/serverConfig.json";
