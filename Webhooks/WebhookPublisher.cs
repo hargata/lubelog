f4e3b4bbe6dc64402de472cdbad5b0497bb4ab8b
Fin Horsley <finhorsley@Fins-MacBook-Air-2.local>
2025-08-26 21:44:49 +0100

    Logging

--- Webhooks/WebhookPublisher.cs
+++ Webhooks/WebhookPublisher.cs
@@ -7,15 +7,19 @@ public class WebhookPublisher : IWebhookPublisher
 {
     private readonly IConfigHelper _config;
     private readonly HttpClient _client;
+    private readonly ILogger<WebhookPublisher> _logger;
 
-    public WebhookPublisher(IConfigHelper config)
+    public WebhookPublisher(IConfigHelper config, ILogger<WebhookPublisher> logger)
     {
         _config = config;
+        _logger = logger;
         _client = new HttpClient();
     }
 
     public async Task PublishAsync(WebHookPayloadBase payload, CancellationToken ct = default)
     {
+        this._logger.LogInformation("Publishing webhook payload");
+
         await _client.PostAsJsonAsync(_config.GetWebHookUrl(), payload, ct);
     }
 }
