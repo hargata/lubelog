f3fcc1700f7ef904faf0a1e9a9ecb5c6b4145b80
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2025-10-31 09:36:35 -0600

    inspection objects

---
+++ Models/InspectionRecord/InspectionRecordInput.cs
@@ -0,0 +1,38 @@
+﻿namespace CarCareTracker.Models
+{
+    public class InspectionRecordInput
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public string Date { get; set; } = DateTime.Now.ToShortDateString();
+        public int Mileage { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public List<int> ReminderRecordId { get; set; } = new List<int>();
+        public List<UploadedFiles> Files { get; set; } = new List<UploadedFiles>();
+        public List<string> Tags { get; set; } = new List<string>();
+        public List<InspectionRecordResult> Results { get; set; } = new List<InspectionRecordResult>();
+        public InspectionRecord ToInspectionRecord()
+        {
+            return new InspectionRecord
+            {
+                Id = Id,
+                VehicleId = VehicleId,
+                Date = DateTime.Parse(Date),
+                Cost = Cost,
+                Mileage = Mileage,
+                Description = Description,
+                Results = Results,
+                Files = Files,
+                Tags = Tags,
+            };
+        }
+    }
+    public class InspectionRecordResult
+    {
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public string Failed { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Models/InspectionRecord/InspectionRecordTemplate.cs
@@ -0,0 +1,27 @@
+﻿namespace CarCareTracker.Models
+{
+    public class InspectionRecordTemplate
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public string Description { get; set; }
+        public List<InspectionRecordTemplateField> Fields { get; set; } = new List<InspectionRecordTemplateField>();
+        public List<int> ReminderRecordId { get; set; } = new List<int>();
+    }
+    
+    public class InspectionRecordTemplateField
+    {
+        public string Description { get; set; }
+        public InspectionFieldType FieldType { get;set; }
+        public List<InspectionRecordTemplateFieldOption> Options { get; set; } = new List<InspectionRecordTemplateFieldOption>();
+        public ImportMode ActionItemType { get; set; }
+        public string ActionItemDescription { get; set; }
+        public PlanPriority ActionItemPriority { get; set; }
+        public bool HasNotes { get; set; }
+    }
+    public class InspectionRecordTemplateFieldOption
+    {
+        public string Description { get; set; }
+        public bool IsFail { get; set; }
+    }
+}
