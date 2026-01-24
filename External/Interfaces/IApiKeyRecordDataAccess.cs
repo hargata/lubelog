8151747b669486d6caa4753ea13b58036dc0a080
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2026-01-23 22:13:06 -0700

    apiKey stuff

---
+++ External/Interfaces/IApiKeyRecordDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using CarCareTracker.Models;
+
+namespace CarCareTracker.External.Interfaces
+{
+    public interface IApiKeyRecordDataAccess
+    {
+        public List<APIKey> GetAPIKeyRecordsByUserId(int userId);
+        public APIKey GetAPIKeyById(int apiKeyId);
+        public APIKey GetAPIKeyByKey(string hashedKey);
+        public bool CreateNewAPIKey(APIKey apiKey);
+        public bool DeleteAPIKey(int apiKeyId);
+    }
+}
---
+++ Models/API/APIKey.cs
@@ -0,0 +1,11 @@
+﻿namespace CarCareTracker.Models
+{
+    public class APIKey
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public List<HouseholdPermission> Permissions { get; set; } = new List<HouseholdPermission>();
+    }
+}
