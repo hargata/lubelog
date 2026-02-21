f6d382a42701e1c57d24e7d6676b999d9618241c
DESKTOP-T0O5CDB\DESK-555BD <ivancheahkf@gmail.com>
2026-02-21 15:01:00 -0700

    add random modifier to prevent async hits.

--- Logic/EventLogic.cs
+++ Logic/EventLogic.cs
@@ -12,9 +12,7 @@ namespace CarCareTracker.Logic
         private readonly IConfigHelper _config;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILogger<EventLogic> _logger;
-        private bool doDelay = false;
-        private int delaySeconds = 2;
-        private int maxDelay = 60;
+        private List<int> delayDict = new List<int>() { 2, 4, 16, 32, 60 };
         public EventLogic(IConfigHelper config, IHttpClientFactory httpClientFactory, ILogger<EventLogic> logger)
         {
             _config = config;
@@ -25,20 +23,15 @@ namespace CarCareTracker.Logic
         {
             string webhookURL = _config.GetWebHookUrl();
             int maxRetries = 5;
-            if (doDelay)
-            {
-                _logger.LogWarning($"WebHook: Delaying for {delaySeconds} seconds before attempt");
-                await Task.Delay(delaySeconds * 1000);
-            }
             if (!string.IsNullOrWhiteSpace(webhookURL))
             {
                 var _httpClient = _httpClientFactory.CreateClient();
                 HttpResponseMessage result;
                 int attempt = 1;
                 bool succeed = false;
-                int iterationDelay = 2;
                 while (!succeed && attempt <= maxRetries)
                 {
+                    int iterationDelay = delayDict[attempt - 1];
                     if (webhookURL.StartsWith("discord://"))
                     {
                         var cleanWebhookURL = webhookURL.Replace("discord://", "https://");
@@ -51,33 +44,23 @@ namespace CarCareTracker.Logic
                     if (result.IsSuccessStatusCode)
                     {
                         succeed = true;
-                        doDelay = false;
-                        delaySeconds = 2; //succeeded, reset delay back to 2
                     }
                     else if (result.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
                     {
-                        doDelay = true;
+                        int randomModifier = new Random().Next(1, 20);
                         //attempt to get retry-after from header
                         if (result.Headers.RetryAfter != null)
                         {
                             if (result.Headers.RetryAfter.Delta.HasValue)
                             {
-                                iterationDelay = (int)result.Headers.RetryAfter.Delta.Value.TotalSeconds;
+                                iterationDelay += (int)Math.Ceiling(result.Headers.RetryAfter.Delta.Value.TotalSeconds);
                             }
                         }
-                        else
-                        {
-                            iterationDelay += (attempt * 2);
-                            if (iterationDelay > maxDelay)
-                            {
-                                iterationDelay = maxDelay;
-                            }
-                        }
+                        iterationDelay += randomModifier;
                         _logger.LogWarning($"WebHook: Too Many Requests, Delaying for {iterationDelay} seconds before attempt {attempt}/{maxRetries}");
                         await Task.Delay(iterationDelay * 1000);
                     } else
                     {
-                        doDelay = true;
                         _logger.LogWarning($"WebHook Error: {result.StatusCode} Attempt {attempt}");
                     }
                     attempt++;
@@ -85,11 +68,6 @@ namespace CarCareTracker.Logic
                 if (!succeed)
                 {
                     _logger.LogWarning($"WebHook Error: Exhausted All Attempts");
-                    delaySeconds += 2; //push back future webhook attempts;
-                    if (delaySeconds > maxDelay)
-                    {
-                        delaySeconds = maxDelay; //cap max delay to 60 seconds.
-                    }
                 }
             }
         }
